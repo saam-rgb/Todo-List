3f4e44e20c90abada6cee95603d98c75a2702b9c
saam <saamsallvin@gmail.com>
2024-05-27 21:34:09 +0530

    Delete Button added

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,14 +1,68 @@
+import { useState } from "react";
 import "./CSS/Todo.css";
+import { useRef } from "react";
+import { useEffect } from "react";
+import TodoItems from "./TodoItems";
 
+let count = 0;
 const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+
+  const add = () => {
+    setTodos([
+      ...todos,
+      { no: count++, text: inputRef.current.value, display: "" },
+    ]);
+    inputRef.current.value = "";
+    localStorage.setItem("todos_count", count);
+  };
+
+  //To fetch dthe data stored in local storage
+  useEffect(() => {
+    setTodos(JSON.parse(localStorage.getItem("todos")));
+    count = localStorage.getItem("todos_count");
+  }, []);
+
+  // To store the data in local storage
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(todos);
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, 100);
+  }, [todos]);
+
   return (
     <div className="todo">
       <div className="todo-header">Todo List</div>
       <div className="todo-add">
-        <input type="text" placeholder="Add your task" className="todo-input" />
-        <div className="todo-add-btn">ADD</div>
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Add your task"
+          className="todo-input"
+        />
+        <div
+          onClick={() => {
+            add();
+          }}
+          className="todo-add-btn">
+          ADD
+        </div>
       </div>
-      <div className="todo-list"></div>
+      <div className="todo-list">
+        {todos.map((item, index) => {
+          return (
+            <TodoItems
+              setTodos={setTodos}
+              key={index}
+              no={item.no}
+              text={item.text}
+              display={item.display}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
